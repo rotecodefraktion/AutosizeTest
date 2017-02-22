b78544780d35ee69d670c88ea00d853673ea8c3f
David Krcek <dkr@Davids-MacBook-Pro.local>
2017-02-22 10:21:45 +0100

    Committer: David Krcek <dkr@Davids-MacBook-Pro.local>
     Changes to be committed:
    	new file:   Autosizing Test/SubUnternehmen.swift
    
     Changes not staged for commit:
    	modified:   Autosizing Test.xcodeproj/project.pbxproj
    	modified:   Autosizing Test/Base.lproj/Main.storyboard
    
     Untracked files:
    	.gitignore

---
+++ Autosizing Test/SubUnternehmen.swift
@@ -0,0 +1,16 @@
+//
+//  SubUnternehmen.swift
+//  Autosizing Test
+//
+//  Created by David Krcek on 17.02.17.
+//  Copyright © 2017 David Krcek. All rights reserved.
+//
+
+import Foundation
+
+protocol Subunternehmen {
+    
+    func rechnungStellung()
+
+
+}
